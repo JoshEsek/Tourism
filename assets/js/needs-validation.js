723b70ec985bbf61bcd9bf101f7cad51ff9b6253
JoshEsek <101793480+JoshEsek@users.noreply.github.com>
2022-03-29 15:56:57 +0100

    Add files via upload

---
+++ assets/js/needs-validation.js
@@ -0,0 +1,31 @@
+/**
+ * Example starter JavaScript for disabling form submissions if there are invalid fields
+ *
+ * @package    roli
+ * @subpackage Queue_Management_System
+ * @since      Queue Management System 1.1
+ *
+ * Core jQueries
+ */
+(function ()
+{
+    "use strict";
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    let forms = document.querySelectorAll(".needs-validation");
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms).forEach(function (form)
+    {
+        form.addEventListener("submit", function (event)
+        {
+            if (!form.checkValidity())
+            {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+
+            form.classList.add("was-validated");
+        }, false);
+    });
+})();
